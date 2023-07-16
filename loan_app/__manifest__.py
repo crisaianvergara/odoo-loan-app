0260448f7d3fdbe46dbd383a9bbd04e56b8cd456
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-16 22:28:51 +0800

    [Add] Main app starting code.

---
+++ loan_app/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Loan Management",
+    "summary": "Mange loan.",
+    "description": "Manage loan.",
+    "author": "Cris-aian Vergara",
+    "website": "https://www.crisaianvergara.com",
+    "category": "Loan",
+    "version": "16.0.1.0.0",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    "sequence": -100,
+    "application": True,
+    "license": "LGPL-3"
+}
---
+++ loan_borrower/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Loan Borrowers",
+    "summary": "Manage borrowers borrowing money.",
+    "description": "Manage borrowers borrowing money.",
+    "author": "Cris-aian Vergara",
+    "website": "https://www.crisaianvergara.com",
+    "category": "Loan",
+    "version": "16.0.1.0.0",
+    "depends": ["loan_app"],
+    "data": [],
+    "demo": [],
+    "sequence": -99,
+    "application": True,
+    "license": "LGPL-3"
+}
